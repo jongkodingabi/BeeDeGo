f8556cf75a563b2a7fba809f71778ed6c80c9c1a
jongkodingabi <abisamhazim04@gmail.com>
2025-03-14 12:54:25 +0700

    fixing hero section

--- src/components/Testimoni.vue
+++ src/components/Testimoni.vue
@@ -36,30 +36,6 @@
         </swiper-slide>
       </swiper>
     </div>
-    <div
-      class="absolute bottom-0 left-0 z-[9999] h-1/3 w-full border-b"
-      style="
-        background-image: linear-gradient(
-          to right top,
-          rgba(79, 70, 229, 0.2) 0%,
-          transparent 50%,
-          transparent 100%
-        );
-        border-color: rgba(92, 79, 240, 0.2);
-      "
-    ></div>
-    <div
-      class="absolute bottom-0 right-0 z-2 h-1/3 w-full"
-      style="
-        background-image: linear-gradient(
-          to left top,
-          rgba(220, 38, 38, 0.2) 0%,
-          transparent 50%,
-          transparent 100%
-        );
-        border-color: rgba(92, 79, 240, 0.2);
-      "
-    ></div>
   </section>
 </template>
 
